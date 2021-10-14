b1aa925241525fc24efeba6944c8fefb4784c868
chrisgray-vertex <chris.gray@vertexvis.com>
2021-10-14 16:32:47 -0500

    publish consul bridge

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@ unzip -q /tmp/awscliv2.zip -d /tmp/
 /tmp/aws/install
 
 mvn -U clean compile package install -Dmaven.test.skip=true -Dmaven.repo.local=./m2
-aws s3 --profile ops cp m2/io/vertx/vertx-service-discovery/ s3://vertex-code-ops-us-west-2/m2/release/io/vertx/vertx-service-discovery/ --recursive --exclude _remote.repositories
+aws s3 --profile ops cp m2/io/vertx/vertx-service-discovery-bridge-consul/ s3://vertex-code-ops-us-west-2/m2/release/io/vertx/vertx-service-discovery-bridge-consul/ --recursive --exclude _remote.repositories
